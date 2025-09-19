39842326138d6ab81c21762a07233ff45c49683c
Marcelo Trylesinski <marcelotryle@gmail.com>
2025-09-19 18:29:48 +0200

    chore: drop `HarmonyAPI` (#51)

--- gateway/src/api/harmony.ts
+++
@@ -1,7 +0,0 @@
-/** This module implements the OpenAI Harmony API.
- * @see https://github.com/openai/harmony
- */
-
-import { BaseAPI } from './base'
-
-export class HarmonyAPI extends BaseAPI<object, object> {}
--- gateway/src/api/index.ts
+++ gateway/src/api/index.ts
@@ -1,7 +1,6 @@
 import { AnthropicAPI } from './anthropic'
 import { ChatCompletionAPI } from './chat'
 import { GoogleAPI } from './google'
-import { HarmonyAPI } from './harmony'
 import { ResponsesAPI } from './responses'
 
-export type ModelAPI = AnthropicAPI | ChatCompletionAPI | HarmonyAPI | ResponsesAPI | GoogleAPI
+export type ModelAPI = AnthropicAPI | ChatCompletionAPI | ResponsesAPI | GoogleAPI

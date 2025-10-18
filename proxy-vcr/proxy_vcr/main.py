07074dac6171dc25a9d9e6cb00958fb2c27cc2aa
Marcelo Trylesinski <marcelotryle@gmail.com>
2025-10-18 18:48:50 +0200

    test(vcr): hide AWS secret on headers

--- proxy-vcr/proxy_vcr/main.py
+++ proxy-vcr/proxy_vcr/main.py
@@ -29,7 +29,7 @@ vcr = VCR(
     cassette_library_dir=(current_file_dir / 'cassettes').as_posix(),
     record_mode=RecordMode.ONCE,
     match_on=['uri', 'method'],
-    filter_headers=['Authorization', 'x-api-key'],
+    filter_headers=['Authorization', 'x-api-key', 'x-amz-security-token'],
 )
 
 

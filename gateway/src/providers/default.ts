ca1e35e0c996587d57c4cef242d75c2e832f7399
Marcelo Trylesinski <marcelotryle@gmail.com>
2025-10-18 15:47:23 +0200

    fix(proxy): drop org/proj identifiers from response headers (#74)

--- gateway/src/providers/default.ts
+++ gateway/src/providers/default.ts
@@ -201,7 +201,9 @@ export class DefaultProviderProxy {
     return null
   }
 
-  protected responseHeaders(_headers: Headers): void {}
+  protected responseHeaders(headers: Headers): Headers {
+    return new Headers(headers)
+  }
 
   protected injectCost(responseBody: JsonData, cost: number) {
     if (this.usageField && this.usageField in responseBody) {
@@ -245,6 +247,9 @@ export class DefaultProviderProxy {
     const { requestBodyText, requestBodyData, requestModel } = prepResult
     const response = await this.fetch(url, { method, headers: requestHeaders, body: requestBodyText })
 
+    // Each provider should be able to modify the response headers, e.g. remove openai org
+    const responseHeaders = this.responseHeaders(response.headers)
+
     if (!response.ok) {
       // CAUTION: can we be charged in any way for failed requests?
       const responseBody = await response.text()
@@ -252,7 +257,7 @@ export class DefaultProviderProxy {
         requestModel,
         requestBody: requestBodyText,
         unexpectedStatus: response.status,
-        responseHeaders: response.headers,
+        responseHeaders,
         responseBody,
       }
     }
@@ -263,10 +268,7 @@ export class DefaultProviderProxy {
     }
     const { responseBody, usage, responseModel, cost } = processResponse
 
-    // TODO we will want to remove some response headers, e.g. openai org
-    const responseHeaders = new Headers(response.headers)
     responseHeaders.set('pydantic-ai-gateway-price-estimate', `${cost.toFixed(4)}USD`)
-    this.responseHeaders(responseHeaders)
 
     if (this.providerProxy.injectCost) {
       this.injectCost(responseBody, cost)

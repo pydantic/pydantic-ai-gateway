70656e2fec4ee2191d691cd50e391d54401c612d
Marcelo Trylesinski <marcelotryle@gmail.com>
2025-10-22 14:20:56 +0200

    Add test for google (#84)

---
+++ gateway/test/providers/google.spec.ts
@@ -0,0 +1,42 @@
+import AnthropicVertex from '@anthropic-ai/sdk'
+import { GoogleGenAI } from '@google/genai'
+import { describe, expect } from 'vitest'
+import { test } from '../setup'
+
+describe('google', () => {
+  // TODO(Marcelo): When Google supports `fetch` parameter, we can fix this: https://github.com/googleapis/js-genai/issues/999
+  test.fails('should call google via gateway', async ({ gateway }) => {
+    const { otelBatch } = gateway
+
+    // The `authToken` is passed as `Authorization` header with the anthropic client.
+    const client = new GoogleGenAI({
+      apiKey: 'healthy',
+      httpOptions: { baseUrl: 'https://example.com/google-vertex' },
+    })
+
+    const response = await client.models.generateContent({
+      model: 'gemini-2.5-flash',
+      contents: 'What is the capital of france?',
+      config: { maxOutputTokens: 1024, topP: 0.95, topK: 1, temperature: 0.5, stopSequences: ['potato'] },
+    })
+
+    expect(response).toMatchSnapshot('llm')
+    expect(otelBatch, 'otelBatch length not 1').toHaveLength(1)
+    expect(JSON.parse(otelBatch[0]!).resourceSpans?.[0].scopeSpans?.[0].spans?.[0]?.attributes).toMatchSnapshot('span')
+  })
+
+  test('should call anthropic via gateway', async ({ gateway }) => {
+    const { fetch, otelBatch } = gateway
+
+    const client = new AnthropicVertex({ baseURL: 'https://example.com/anthropic', fetch, authToken: 'healthy' })
+
+    const completion = await client.messages.create({
+      model: 'claude-sonnet-4-20250514',
+      max_tokens: 1024,
+      messages: [{ role: 'user', content: 'What is the capital of France?' }],
+    })
+    expect(completion).toMatchSnapshot('llm')
+    expect(otelBatch, 'otelBatch length not 1').toHaveLength(1)
+    expect(JSON.parse(otelBatch[0]!).resourceSpans?.[0].scopeSpans?.[0].spans?.[0]?.attributes).toMatchSnapshot('span')
+  })
+})

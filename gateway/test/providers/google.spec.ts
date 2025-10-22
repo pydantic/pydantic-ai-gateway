f04f8ec287a5928a91c1a82d1f96dc416cd042d8
Marcelo Trylesinski <marcelotryle@gmail.com>
2025-10-22 14:19:23 +0200

    Add test for anthropic on vertex

--- gateway/test/providers/google.spec.ts
+++ gateway/test/providers/google.spec.ts
@@ -1,10 +1,12 @@
+import AnthropicVertex from '@anthropic-ai/sdk'
 import { GoogleGenAI } from '@google/genai'
 import { describe, expect } from 'vitest'
 import { test } from '../setup'
 
 describe('google', () => {
-  test('should call google via gateway', async ({ gateway }) => {
-    const { fetch, otelBatch } = gateway
+  // TODO(Marcelo): When Google supports `fetch` parameter, we can fix this: https://github.com/googleapis/js-genai/issues/999
+  test.fails('should call google via gateway', async ({ gateway }) => {
+    const { otelBatch } = gateway
 
     // The `authToken` is passed as `Authorization` header with the anthropic client.
     const client = new GoogleGenAI({
@@ -19,6 +21,21 @@ describe('google', () => {
     })
 
     expect(response).toMatchSnapshot('llm')
+    expect(otelBatch, 'otelBatch length not 1').toHaveLength(1)
+    expect(JSON.parse(otelBatch[0]!).resourceSpans?.[0].scopeSpans?.[0].spans?.[0]?.attributes).toMatchSnapshot('span')
+  })
+
+  test('should call anthropic via gateway', async ({ gateway }) => {
+    const { fetch, otelBatch } = gateway
+
+    const client = new AnthropicVertex({ baseURL: 'https://example.com/anthropic', fetch, authToken: 'healthy' })
+
+    const completion = await client.messages.create({
+      model: 'claude-sonnet-4-20250514',
+      max_tokens: 1024,
+      messages: [{ role: 'user', content: 'What is the capital of France?' }],
+    })
+    expect(completion).toMatchSnapshot('llm')
     expect(otelBatch, 'otelBatch length not 1').toHaveLength(1)
     expect(JSON.parse(otelBatch[0]!).resourceSpans?.[0].scopeSpans?.[0].spans?.[0]?.attributes).toMatchSnapshot('span')
   })

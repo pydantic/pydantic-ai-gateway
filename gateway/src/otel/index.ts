b97ccbc25a589aa742f7062412d8ead4bd897a56
Marcelo Trylesinski <marcelotryle@gmail.com>
2025-10-27 09:06:28 +0100

    Revert "Allow use of Logfire staging write token (#100)"
    This reverts commit 7636f1e44d654bb53f70a1a1c59328c035577be1.

--- gateway/src/otel/index.ts
+++ gateway/src/otel/index.ts
@@ -316,9 +316,6 @@ function getBaseUrl({ baseUrl, writeToken }: OtelSettings): string | undefined {
   if (baseUrl) {
     return baseUrl
   }
-  const regionMatch = /pylf_v\d_(us|eu|stagingeu)/.exec(writeToken)
-  if (regionMatch?.[1] === 'stagingeu') {
-    return 'https://logfire-eu.pydantic.info'
-  }
+  const regionMatch = /pylf_v\d_(us|eu)/.exec(writeToken)
   return regionMatch?.[1] === 'eu' ? 'https://api-eu.logfire.dev' : 'https://api.logfire.dev'
 }

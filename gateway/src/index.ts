36723c55c6551321fb043be790307333148c362f
Marcelo Trylesinski <marcelotryle@gmail.com>
2025-11-11 19:05:16 +0100

    pass everything

--- gateway/src/index.ts
+++ gateway/src/index.ts
@@ -1,3 +1,20 @@
+/*
+Copyright (C) 2025 to present Pydantic Services Inc.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+import * as logfire from '@pydantic/logfire-api'
 import type { KeysDb, LimitDb } from './db'
 import { gateway } from './gateway'
 import type { DefaultProviderProxy, Middleware, Next } from './providers/default'
@@ -35,12 +52,17 @@ export async function gatewayFetch(
   if (options.proxyPrefixLength) {
     proxyPath = proxyPath.slice(options.proxyPrefixLength)
   }
-  if (proxyPath === '/') {
-    return index(request, options)
-  } else {
-    const gatewayPromise = gateway(request, `${proxyPath}${queryString}`, ctx, options)
-    runAfter(ctx, 'gatewayPromise', gatewayPromise)
-    return await gatewayPromise
+  try {
+    if (proxyPath === '/') {
+      return index(request, options)
+    } else {
+      const gatewayPromise = gateway(request, `${proxyPath}${queryString}`, ctx, options)
+      runAfter(ctx, 'gatewayPromise', gatewayPromise)
+      return await gatewayPromise
+    }
+  } catch (error) {
+    logfire.reportError('ResponseError', error as Error)
+    throw error
   }
 }
 

61cce7e7607e7f011f11e31aaf7f9936eda5b929
Marcelo Trylesinski <marcelotryle@gmail.com>
2025-11-17 11:07:58 +0000

    feat(openai): support embeddings

---
+++ gateway/src/api/embeddings.ts
@@ -0,0 +1,4 @@
+import type { EmbeddingCreateParams, Embeddings } from 'openai/resources/embeddings'
+import { BaseAPI } from './base'
+
+export class EmbeddingsAPI extends BaseAPI<EmbeddingCreateParams, Embeddings> {}

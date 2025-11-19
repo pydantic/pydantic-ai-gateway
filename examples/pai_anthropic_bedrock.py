d097812918d74ae0be07ceac9db415ccf871c323
Marcelo Trylesinski <marcelotryle@gmail.com>
2025-11-19 11:26:21 +0000

    feat(bedrock): support fallback from anthropic (#155)

---
+++ examples/pai_anthropic_bedrock.py
@@ -0,0 +1,44 @@
+import os
+from datetime import date
+
+import logfire
+from pydantic import BaseModel, field_validator
+from pydantic_ai import Agent
+from pydantic_ai.models.anthropic import AnthropicModel, AnthropicModelSettings
+from pydantic_ai.providers.gateway import gateway_provider
+
+logfire.configure(service_name='testing')
+logfire.instrument_pydantic_ai()
+logfire.instrument_httpx(capture_all=True)
+
+
+class Person(BaseModel, use_attribute_docstrings=True):
+    name: str
+    """The name of the person."""
+    dob: date
+    """The date of birth of the person. MUST BE A VALID ISO 8601 date."""
+    city: str
+    """The city where the person lives."""
+
+    @field_validator('dob')
+    def validate_dob(cls, v: date) -> date:
+        if v >= date(1900, 1, 1):
+            raise ValueError('The person must be born in the 19th century')
+        return v
+
+
+api_key = os.getenv('PYDANTIC_AI_GATEWAY_API_KEY')
+assert api_key is not None
+
+model = AnthropicModel(
+    'claude-sonnet-4',
+    provider=gateway_provider('anthropic', route='bedrock', base_url='http://localhost:8787/', api_key=api_key),
+)
+person_agent = Agent(
+    model,
+    output_type=Person,
+    instructions='Extract information about the person',
+    model_settings=AnthropicModelSettings(max_tokens=1024),
+)
+result = person_agent.run_sync("Samuel lived in London and was born on Jan 28th '87")
+print(repr(result.output))
